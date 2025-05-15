5708b6f71c27704e2a30b510da26cdbd9d815757
AugieAud <auds@missionreadyhq.com>
2025-05-15 12:15:57 +1200

    add comments to log.js and test everything

--- log.js
+++ log.js
@@ -4,8 +4,8 @@ const path = require("path");
 const gitDir = path.join(process.cwd(), ".mygit");
 
 const log = () => {
-  const head = fs.readFileSync(path.join(gitDir, "HEAD"), "utf8").trim();
-  if (!head) return console.log("No commits yet.");
+  const head = fs.readFileSync(path.join(gitDir, "HEAD"), "utf8").trim(); //reads commit history stored in HEAD
+  if (!head) return console.log("No commits yet."); //checks for commits and logs appropriate message
 
   const content = fs.readFileSync(path.join(gitDir, "commits", head), "utf8");
   console.log(`Commit: ${head}`);
